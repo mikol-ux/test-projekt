1a76ac0dfbe32c352326cb67156121d4e2c6115e
Gausoddin <gausoddinpirjade@gmail.com>
2025-01-24 10:42:03 +0530

    [aboutUs] removed duplicate footer & navbar

--- src/app/(home)/(pages)/AboutUs/components/Navbar/TheBeginning.tsx
+++
@@ -1,9 +0,0 @@
-import TheBeginning from '@/app/(home)/partials/TheBeginning/TheBeginning';
-
-export default function Navbar() {
-  return (
-    <nav className="navbar">
-      <TheBeginning />
-    </nav>
-  );
-}
--- src/app/(home)/(pages)/AboutUs/layout.tsx
+++ src/app/(home)/(pages)/AboutUs/layout.tsx
@@ -1,13 +1,9 @@
-import Navbar from "./components/Navbar/TheBeginning";
-import Footer from "./components/Footer/TheEnding";
 import { Fragment } from "react";
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
     return (
         <Fragment>
-            <Navbar />
             <main>{children}</main>
-            <Footer />
         </Fragment>
     );
 }
