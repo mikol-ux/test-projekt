180b3ef1a49f870fe9610bebb126c5e0f8994115
mikol-ux <okoyeikechukwu60@gmail.com>
2025-02-11 13:50:14 -0800

    used new classes and fixed service section

--- src/app/(home)/sections/TheService/index.tsx
+++ src/app/(home)/sections/TheService/index.tsx
@@ -1,80 +1,86 @@
 import GradientBtn from "@/components/Buttons/GradientBtn";
 import Link from "next/link";
 
-interface AboutUsProps { }
+interface TheServiceProps {}
 
-const AboutUs: React.FC<AboutUsProps> = () => {
-	const options: string[] = [
-		"(A) FULL-SERVICE WEB & CYBER SECURITY DEVELOPMENT COMPANY",
-		"(B) UX/UI DESIGNERS AND BRAND STRATEGISTS",
-		"(C) COMMUNITY OF DREAMERS TURNED ENTREPRENEURS",
-		"(D) MULTIMEDIA CREATIVE STUDIO",
-		"(E) WE ARE TALENT ACQUISITION SPECIALISTS - THE WATCHMEN",
-	];
+const TheService: React.FC<TheServiceProps> = () => {
+  const options: string[] = [
+    "(A) FULL-SERVICE WEB & CYBER SECURITY DEVELOPMENT COMPANY",
+    "(B) UX/UI DESIGNERS AND BRAND STRATEGISTS",
+    "(C) COMMUNITY OF DREAMERS TURNED ENTREPRENEURS",
+    "(D) MULTIMEDIA CREATIVE STUDIO",
+    "(E) WE ARE TALENT ACQUISITION SPECIALISTS - THE WATCHMEN",
+  ];
 
-	return (
-		<section className="flex flex-col items-center justify-center tpk-contained">
-			<div className="flex flex-col md:flex-row justify-start gap-10 md:gap-0">
-				<div className="md:basis-3/12 flex flex-row items-start justify-start gap-3">
-					<h4 className="text-black dark:text-muted-5 tpk-text-md font-normal uppercase md:mt-4">
-						Who Are We
-					</h4>
-				</div>
-				<div className="md:basis-9/12 flex flex-col gap-6">
-					<h2 className="text-black dark:text-white text-start tpk-text-2xl leading-relaxed font-normal uppercase">
-						WE ARE A WEB DEVELOPMENT & CYBER SECURITY AGENCY
-					</h2>
-					<p className="text-muted-4 dark:text-muted text-start tpk-text-lg">
-						At The Projekt, we know what it&apos;s like to take big risks
-						without having a huge budget and clarity. That&apos;s why we created
-						the Foundation Program—designed for the dreamers, misfits, and
-						rebels who are too busy chasing something bigger. It&apos;s not just
-						about launching your business; it&apos;s about growing to the point
-						where you&apos;re turning clients away because you&apos;re in such
-						high demand.
-					</p>
-				</div>
-			</div>
+  return (
+    <section className="flex flex-col items-center justify-center tpk-contained">
+      <div className="flex flex-col md:flex-row justify-start gap-10 md:gap-0">
+        <div className="md:basis-3/12 flex flex-row items-start justify-start gap-3">
+          <h4 className="text-black dark:text-muted-5 tpk-text-md font-normal uppercase md:mt-4">
+            Who Are We
+          </h4>
+        </div>
+        <div className="md:basis-9/12 flex flex-col gap-6">
+          <h2 className="text-black dark:text-white text-start tpk-text-2xl leading-relaxed font-normal uppercase">
+            WE ARE A WEB DEVELOPMENT & CYBER SECURITY AGENCY
+          </h2>
+          <p className="text-muted-4 dark:text-muted text-start tpk-text-lg">
+            At The Projekt, we know what it&apos;s like to take big risks
+            without having a huge budget and clarity. That&apos;s why we created
+            the Foundation Program—designed for the dreamers, misfits, and
+            rebels who are too busy chasing something bigger. It&apos;s not just
+            about launching your business; it&apos;s about growing to the point
+            where you&apos;re turning clients away because you&apos;re in such
+            high demand.
+          </p>
+        </div>
+      </div>
 
-			{/* Options */}
+      {/* Options */}
 
-			<div className="w-full mt-8">
-				<ul className="grid grid-rows-5">
-					{options.map((option, index, arrLen) => (
-						<li
-							key={index}
-							className={`group bg-linear-to-r from-pitch-dark-blue from-[-4.81%] via-deep-blue via-[27%] to-sky-blue to-100% p-[1px] ${index < arrLen.length - 1? "pb-0": ''} rounded-lg cursor-pointer transition-background duration-300 md:items-center justify-start gap-2 md:gap-10`}>
-							{/* Icon & Text */}
-							<div className="flex flex-col md:flex-row rounded-lg h-full ps-4 md:ps-0 py-4 lg:py-5 xl:py-7 2xl:py-10 bg-black hover:bg-gradient-to-r hover:from-black hover:to-deep-blue hover:text-white tpk-text-lg font-normal">
-								<div className="basis-3/12 flex md:items-center justify-start gap-8 md:gap-16 text-black dark:text-white uppercase transition-opacity duration-500 md:px-8 lg:px-16 opacity-0 group-hover:opacity-100">
-									<span>
-										<svg width="23"
-											height="22"
-											viewBox="0 0 23 22"
-											fill="none"
-											xmlns="http://www.w3.org/2000/svg"
-										>
-											<path
-												d="M22.5 10.1639V11.8361C22.5 12.0229 22.348 12.1749 22.1612 12.1793C19.7505 12.2618 17.5701 13.2043 15.9109 14.7246C15.8544 14.7724 15.7979 14.8245 15.7458 14.8766C15.698 14.92 15.6502 14.9635 15.6068 15.0113C15.5764 15.0417 15.546 15.0721 15.5113 15.1068C15.4678 15.1502 15.4244 15.198 15.3766 15.2458C13.7738 16.9224 12.7661 19.168 12.6793 21.6612C12.6749 21.848 12.5229 22 12.3361 22H10.6639C10.4771 22 10.3251 21.848 10.3207 21.6612C10.2425 19.2592 9.29566 17.0831 7.78845 15.4239C7.6842 15.3066 7.57562 15.1937 7.46703 15.0851C7.44965 15.0677 7.43662 15.0547 7.41925 15.0373C7.31066 14.9287 7.19339 14.8201 7.08045 14.7159C7.01965 14.6638 6.96318 14.6116 6.90237 14.5639C5.26486 13.1435 3.15824 12.2618 0.834452 12.1836C0.64768 12.1793 0.5 12.0316 0.5 11.8448V10.1682C0.5 9.98144 0.652024 9.82942 0.838796 9.82507C3.32764 9.7382 5.57759 8.7305 7.25419 7.12774C9.22616 5.07759 10.2382 2.83198 10.3207 0.343139C10.3251 0.152024 10.4771 0 10.6639 0H12.3361C12.5229 0 12.6749 0.152024 12.6793 0.338796C12.7661 2.82764 13.7695 5.0689 15.3679 6.74551C17.7178 8.85212 19.8331 9.7382 22.1612 9.81639C22.348 9.82073 22.4957 9.96841 22.4957 10.1552L22.5 10.1639Z"
-												fill="#808080"
-											/>
-										</svg>
-									</span>
-									<span>WE ARE</span>
-								</div>
-								<div className="basis-9/12 flex text-sky-blue">
-									<p>{option}</p>
-								</div>
-							</div>
-						</li>
-					))}
-				</ul>
-			</div>
-			<div className="block w-full mt-28 text-center">
-				<GradientBtn href="/about" className="tpk-text-md">I Want The Full Story</GradientBtn>
-			</div>
-		</section>
-	);
+      <div className="w-full mt-8">
+        <ul className="grid grid-rows-5">
+          {options.map((option, index, arrLen) => (
+            <li
+              key={index}
+              className={`group bg-linear-to-r from-pitch-dark-blue from-[-4.81%] via-deep-blue via-[27%] to-sky-blue to-100% p-[1px] ${
+                index < arrLen.length - 1 ? "" : ""
+              } rounded-lg cursor-pointer transition-background duration-1000 ease-in-out md:items-center justify-start gap-2 md:gap-10 mr-1.5 hover:mr-0 hover:p-0`}
+            >
+              {/* Icon & Text */}
+              <div className="flex flex-col md:flex-row rounded-lg h-full ps-4 md:ps-0 py-4 lg:py-5 xl:py-7 2xl:py-10 bg-black hover:bg-gradient-to-r transition-colors duration-1000 ease-in-out hover:from-black hover:to-deep-blue hover:text-white tpk-text-lg font-normal">
+                <div className="basis-3/12 flex md:items-center justify-start gap-8 md:gap-16 text-black dark:text-white uppercase transition-opacity duration-1000 ease-in-out md:px-8 lg:px-16 opacity-0 group-hover:opacity-100">
+                  <span>
+                    <svg
+                      width="23"
+                      height="22"
+                      viewBox="0 0 23 22"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M22.5 10.1639V11.8361C22.5 12.0229 22.348 12.1749 22.1612 12.1793C19.7505 12.2618 17.5701 13.2043 15.9109 14.7246C15.8544 14.7724 15.7979 14.8245 15.7458 14.8766C15.698 14.92 15.6502 14.9635 15.6068 15.0113C15.5764 15.0417 15.546 15.0721 15.5113 15.1068C15.4678 15.1502 15.4244 15.198 15.3766 15.2458C13.7738 16.9224 12.7661 19.168 12.6793 21.6612C12.6749 21.848 12.5229 22 12.3361 22H10.6639C10.4771 22 10.3251 21.848 10.3207 21.6612C10.2425 19.2592 9.29566 17.0831 7.78845 15.4239C7.6842 15.3066 7.57562 15.1937 7.46703 15.0851C7.44965 15.0677 7.43662 15.0547 7.41925 15.0373C7.31066 14.9287 7.19339 14.8201 7.08045 14.7159C7.01965 14.6638 6.96318 14.6116 6.90237 14.5639C5.26486 13.1435 3.15824 12.2618 0.834452 12.1836C0.64768 12.1793 0.5 12.0316 0.5 11.8448V10.1682C0.5 9.98144 0.652024 9.82942 0.838796 9.82507C3.32764 9.7382 5.57759 8.7305 7.25419 7.12774C9.22616 5.07759 10.2382 2.83198 10.3207 0.343139C10.3251 0.152024 10.4771 0 10.6639 0H12.3361C12.5229 0 12.6749 0.152024 12.6793 0.338796C12.7661 2.82764 13.7695 5.0689 15.3679 6.74551C17.7178 8.85212 19.8331 9.7382 22.1612 9.81639C22.348 9.82073 22.4957 9.96841 22.4957 10.1552L22.5 10.1639Z"
+                        fill="#808080"
+                      />
+                    </svg>
+                  </span>
+                  <span>WE ARE</span>
+                </div>
+                <div className="basis-9/12 flex text-sky-blue">
+                  <p>{option}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="block w-full mt-28 text-center">
+        <GradientBtn href="/about" className="tpk-text-md">
+          I Want The Full Story
+        </GradientBtn>
+      </div>
+    </section>
+  );
 };
 
-export default AboutUs;
+export default TheService;
--- src/components/Section-components/theQuestion.tsx
+++ src/components/Section-components/theQuestion.tsx
@@ -1,37 +1,93 @@
-'use client';
+"use client";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 const faqs = [
   {
-    question: 'WHAT SEPARATES THE PROJEKT FROM OTHER COMPANIES?',
-    answer: 'We provide a unique approach tailored to each client, ensuring quality, efficiency, and innovation.',
+    question: "WHAT SEPARATES THE PROJEKT FROM OTHER COMPANIES?",
+    answer:
+      "We provide a unique approach tailored to each client, ensuring quality, efficiency, and innovation.",
   },
   {
-    question: 'DO YOU WORK WITH CLIENTS IN DIFFERENT TIMEZONES?',
+    question: "DO YOU WORK WITH CLIENTS IN DIFFERENT TIMEZONES?",
     answer: (
       <>
-        <p>Yes, we work with clients globally and adjust our schedules to ensure seamless collaboration.</p>
-        <p>In a saturated market of excellent design and branding agencies, it's challenging to stand out. However, we are not a branding or design agency. We are a software development and cyber security first company. We don’t just blindly build a good-looking website for our clients. We dive deep into market research to test whether their idea even makes sense. Everyone thinks their idea is brilliant—until we poke holes in it, find the weaknesses, and help fortify it. If it survives? We give it a name and go from there.</p>
+        <p>
+          Yes, we work with clients globally and adjust our schedules to ensure
+          seamless collaboration.
+        </p>
+        <p>
+          In a saturated market of excellent design and branding agencies, it's
+          challenging to stand out. However, we are not a branding or design
+          agency. We are a software development and cyber security first
+          company. We don’t just blindly build a good-looking website for our
+          clients. We dive deep into market research to test whether their idea
+          even makes sense. Everyone thinks their idea is brilliant—until we
+          poke holes in it, find the weaknesses, and help fortify it. If it
+          survives? We give it a name and go from there.
+        </p>
         <ul className="list-disc list-inside mt-2">
-          <li>When you choose us, you collaborate directly with our dedicated team, get access to founders who have worked with us and genuinely care about your success.</li>
-          <li>Our cofounders oversee every project, while a design director leads the way, ensuring thorough design reviews and involvement in project details.</li>
-          <li>We have a hiring division that allows our clients (partners) who have remote-friendly businesses to hire vetted and qualified off-shore employees.</li>
-          <li>Communication and transparency drive all our interactions, guaranteeing a premium client experience from start to finish. Our agility stems from working with a diverse set of businesses, and our refined project management approach comes from years of experience.</li>
+          <li>
+            When you choose us, you collaborate directly with our dedicated
+            team, get access to founders who have worked with us and genuinely
+            care about your success.
+          </li>
+          <li>
+            Our cofounders oversee every project, while a design director leads
+            the way, ensuring thorough design reviews and involvement in project
+            details.
+          </li>
+          <li>
+            We have a hiring division that allows our clients (partners) who
+            have remote-friendly businesses to hire vetted and qualified
+            off-shore employees.
+          </li>
+          <li>
+            Communication and transparency drive all our interactions,
+            guaranteeing a premium client experience from start to finish. Our
+            agility stems from working with a diverse set of businesses, and our
+            refined project management approach comes from years of experience.
+          </li>
         </ul>
       </>
     ),
   },
   {
-    question: 'HOW MUCH DOES HIRING YOU FOR A CUSTOM PROJEKT COST?',
+    question: "HOW MUCH DOES HIRING YOU FOR A CUSTOM PROJEKT COST?",
     answer: (
       <>
-       <p>In a saturated market of excellent design and branding agencies, it's challenging to stand out. However, we are not a branding or design agency. We are a software development and cyber security first company. We don’t just blindly build a good-looking website for our clients. We dive deep into market research to test whether their idea even makes sense. Everyone thinks their idea is brilliant—until we poke holes in it, find the weaknesses, and help fortify it. If it survives? We give it a name and go from there.</p>
+        <p>
+          In a saturated market of excellent design and branding agencies, it's
+          challenging to stand out. However, we are not a branding or design
+          agency. We are a software development and cyber security first
+          company. We don’t just blindly build a good-looking website for our
+          clients. We dive deep into market research to test whether their idea
+          even makes sense. Everyone thinks their idea is brilliant—until we
+          poke holes in it, find the weaknesses, and help fortify it. If it
+          survives? We give it a name and go from there.
+        </p>
         <ul className="list-disc list-inside mt-2">
-          <li>When you choose us, you collaborate directly with our dedicated team, get access to founders who have worked with us and genuinely care about your success.</li>
-          <li>Our cofounders oversee every project, while a design director leads the way, ensuring thorough design reviews and involvement in project details.</li>
-          <li>We have a hiring division that allows our clients (partners) who have remote-friendly businesses to hire vetted and qualified off-shore employees.</li>
-          <li>Communication and transparency drive all our interactions, guaranteeing a premium client experience from start to finish. Our agility stems from working with a diverse set of businesses, and our refined project management approach comes from years of experience.</li>
+          <li>
+            When you choose us, you collaborate directly with our dedicated
+            team, get access to founders who have worked with us and genuinely
+            care about your success.
+          </li>
+          <li>
+            Our cofounders oversee every project, while a design director leads
+            the way, ensuring thorough design reviews and involvement in project
+            details.
+          </li>
+          <li>
+            We have a hiring division that allows our clients (partners) who
+            have remote-friendly businesses to hire vetted and qualified
+            off-shore employees.
+          </li>
+          <li>
+            Communication and transparency drive all our interactions,
+            guaranteeing a premium client experience from start to finish. Our
+            agility stems from working with a diverse set of businesses, and our
+            refined project management approach comes from years of experience.
+          </li>
         </ul>
       </>
     ),
@@ -52,20 +108,33 @@ const ClientFAQ = () => {
       {faqs.map((faq, index) => (
         <div
           key={index}
-          className={`py-2 text-black dark:text-white border-l border-r border-b-0 border-gray-700 rounded-[4px] transition-all ${openIndexes.includes(index) ? 'bg-gradient-to-r from-[#000000] to-[#438DB5] border-t-0' : 'bg-transparent border-t'}`}
+          className={`group bg-linear-to-r from-pitch-dark-blue from-[-4.81%] via-deep-blue via-[27%] to-sky-blue to-100% p-[1px]
+              rounded-[4px] cursor-pointer `}
         >
-          <button
-            className="w-full flex gap-8 justify-start items-center px-6 py-4 transition-all"
-            onClick={() => toggleFAQ(index)}
+          <div
+            className={`py-2 text-black dark:text-white transition-colors rounded-[4px] ${
+              openIndexes.includes(index)
+                ? "bg-gradient-to-r from-black to-deep-blue"
+                : "bg-black"
+            }`}
           >
-            <span className="text-xl">{openIndexes.includes(index) ? '−' : '+'}</span>
-            <span className="text-left text-xl font-normal">{faq.question}</span>
-          </button>
-          {openIndexes.includes(index) && (
-            <div className="px-6 py-4 text-sm text-black dark:text-white font-light">
-              {faq.answer}
-            </div>
-          )}
+            <button
+              className="w-full flex gap-8 justify-start items-center px-6 py-4 transition-all"
+              onClick={() => toggleFAQ(index)}
+            >
+              <span className="tpk-text-lg">
+                {openIndexes.includes(index) ? "−" : "+"}
+              </span>
+              <span className="text-left tpk-text-lg font-normal">
+                {faq.question}
+              </span>
+            </button>
+            {openIndexes.includes(index) && (
+              <div className="px-6 py-4 tpx-text-sm text-black dark:text-white font-light">
+                {faq.answer}
+              </div>
+            )}
+          </div>
         </div>
       ))}
     </div>
