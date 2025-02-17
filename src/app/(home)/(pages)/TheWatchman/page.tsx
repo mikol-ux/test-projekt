fbc1664fa96538f00eb8d127ac6a4044934890ac
E-E-Ewoma-Zino <eewoma75@gmail.com>
2025-02-17 15:40:22 +0100

    [TheWatchman] Created the watchman dir

---
+++ src/app/(home)/(pages)/TheWatchman/page.tsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react";
+import FindingTalent from "./sections/FindingTalent";
+
+export default function AboutUsPage() {
+	return (
+
+		<main className="flex flex-col gap-32">
+			<FindingTalent />
+		</main>
+	);
+}
---
+++ src/app/(home)/(pages)/TheWatchman/sections/FindingTalent/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FindingTalent = () => {
+	return (
+		<section className="tpk-contained">
+			<div className="flex">
+				<div className="col-12 md:col-6/12"></div>
+				<div className="col-12 md:col-6/12"></div>
+			</div>
+		</section>
+	);
+};
+
+export default FindingTalent;
