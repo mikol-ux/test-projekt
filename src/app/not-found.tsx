6e72e037080a5f5c2dd45ed9372db5782d2fac43
E-E-Ewoma-Zino <eewoma75@gmail.com>
2025-02-18 10:39:18 +0100

    [Renormalize] Some folders were having different casing on git eg components/Buttons was components/buttons

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -13,7 +13,7 @@ export default function NotFound() {
 				</p>
 				<div className="mt-10 flex items-center justify-center gap-x-6">
 					<Link href="/"
-						className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+						className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 					>
 						Go back home
 					</Link>
