08c83c5a7f5d17bb341e0e6b0f0122550176ae65
mikol-ux <okoyeikechukwu60@gmail.com>
2025-02-20 03:47:59 +0100

    new fix

--- src/components/Lottie/index.tsx
+++ src/components/Lottie/index.tsx
@@ -1,18 +1,15 @@
-//
 "use client";
 import { FC } from "react";
 import Lottie from "react-lottie-player";
 
 interface LottieData {
-	json: string;
-	play: boolean;
-	className: string;
+  json: any; // Changed from string to any (should be a JSON object)
+  play: boolean;
+  className?: string; // Made className optional
 }
 
-const LottieAni: FC<LottieData> = ({json, play, className}) => {
-	return (
-		<Lottie path={json} play={play} className={className} />
-	);
-}
+const LottieAni: FC<LottieData> = ({ json, play, className = "" }) => {
+  return <Lottie animationData={json} play={play} className={className} />;
+};
 
 export default LottieAni;
